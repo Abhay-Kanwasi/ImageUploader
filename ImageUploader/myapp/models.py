79f07c8dabec2006e928027c328df9c476e641ee
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2023-09-14 16:59:14 +0530

    cloned

--- ImageUploader/myapp/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-class Image(models.Model):
-    image = models.ImageField(upload_to = "uploaded_images")
-    date = models.DateTimeField(auto_now_add=True)
-    # user = models.ForeignKey(User, on_delete=models.CASCADE, null= True, blank=True)
